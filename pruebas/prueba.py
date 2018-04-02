fa107810c85d45229e006870b962f02fd0c60f76
franky <frankyshelley@gmail.com>
2018-04-02 16:46:37 +0200

    rev2

--- pruebas/prueba.py
+++ pruebas/prueba.py
@@ -35,9 +35,7 @@ except ImportError:
 	print(ROJO +'FALLO CRITICO')
 	print(BLANCO +'fallo módulo motor')
 	sys.exit()
-import math
-import mpu6050
-
+from sensor import sensor
 #######################VARIABLES##############################
 x = 0
 y = 0
@@ -60,21 +58,8 @@ motor4 = 0
 ip = "192.168.1.101"
 puerto = 5000
 ######################INICIO SENSORES##########################
-# Sensor initialization
-mpu = mpu6050.MPU6050()
-mpu.dmpInitialize()
-mpu.setDMPEnabled(True)
-    
-# get expected DMP packet size for later comparison
-packetSize = mpu.dmpGetFIFOPacketSize() 
+gyro = sensor()
 
-# Sensor initialization
-mpu = mpu6050.MPU6050()
-mpu.dmpInitialize()
-mpu.setDMPEnabled(True)
-    
-# get expected DMP packet size for later comparison
-packetSize = mpu.dmpGetFIFOPacketSize() 
 
 #####################MOTORES##################################
 
@@ -91,42 +76,16 @@ for m in motores:
 		
 print(VERDE + '[OK]' + BLANCO + 'motores armados')
 ##############################################################3
-a = input('al cielo:)
-          
-        for m in motores:
-                m.setW(10)
-        while True:
-            # Get INT_STATUS byte
-            mpuIntStatus = mpu.getIntStatus()
-          
-            if mpuIntStatus >= 2: # check for DMP data ready interrupt (this should happen frequently) 
-                # get current FIFO count
-                fifoCount = mpu.getFIFOCount()
-                
-                # check for overflow (this should never happen unless our code is too inefficient)
-                if fifoCount == 1024:
-                    # reset so we can continue cleanly
-                    mpu.resetFIFO()
-                    print('FIFO overflow!')
-                    
-                    
-                # wait for correct available data length, should be a VERY short wait
-                fifoCount = mpu.getFIFOCount()
-                while fifoCount < packetSize:
-                    fifoCount = mpu.getFIFOCount()
-                
-                result = mpu.getFIFOBytes(packetSize)
-                q = mpu.dmpGetQuaternion(result)
-                g = mpu.dmpGetGravity(q)
-                ypr = mpu.dmpGetYawPitchRoll(q, g)
-                
-                print(ypr['yaw'] * 180 / math.pi),
-                print(ypr['pitch'] * 180 / math.pi),
-                print(ypr['roll'] * 180 / math.pi)
-            
-                # track FIFO count here in case there is > 1 packet available
-                # (this lets us immediately read more without waiting for an interrupt)        
-                fifoCount -= packetSize  
-                
+a = input('al cielo:')
+
+    for m in motores:
+        m.setW(10)
+    while True:
+        giro  = sensor.imu()
+        print(giro[0])
+        print(giro[1])
+        print(giro[2])
+
+   
 for m in motores:
         m.stop()
